bb4003284adec6dccf181db6b6db1b20de2ab8f4
nariahlamb <nariahlamb31@gmail.com>
2025-06-29 21:34:17 +0800

    Add electron-vite configuration file

---
+++ electron.vite.config.ts
@@ -0,0 +1,47 @@
+import react from '@vitejs/plugin-react-swc'
+import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  main: {
+    plugins: [externalizeDepsPlugin()],
+    resolve: {
+      alias: {
+        '@main': resolve('src/main'),
+        '@shared': resolve('packages/shared')
+      }
+    },
+    build: {
+      rollupOptions: {
+        external: ['opendal']
+      }
+    }
+  },
+  preload: {
+    plugins: [externalizeDepsPlugin()],
+    resolve: {
+      alias: {
+        '@shared': resolve('packages/shared')
+      }
+    }
+  },
+  renderer: {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@renderer': resolve('src/renderer/src'),
+        '@shared': resolve('packages/shared')
+      }
+    },
+    build: {
+      rollupOptions: {
+        input: {
+          index: resolve(__dirname, 'src/renderer/index.html'),
+          miniWindow: resolve(__dirname, 'src/renderer/miniWindow.html'),
+          selectionToolbar: resolve(__dirname, 'src/renderer/selectionToolbar.html'),
+          selectionAction: resolve(__dirname, 'src/renderer/selectionAction.html')
+        }
+      }
+    }
+  }
+})
